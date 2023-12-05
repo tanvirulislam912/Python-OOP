879066cabc545794e0319546ac8d0dd53b0907f8
tanvirulislam912 <mdtanvir1109040@gmail.com>
2023-12-05 20:44:20 +0600

    first commit

--- Class Object Constructor Attributes Methods().py
+++ Class Object Constructor Attributes Methods().py
@@ -14,17 +14,28 @@ class Student:
 
     def __init__(self,name, Id):                       #Constructor Created
         # print(self)
-        self.name = name                                #Instance Variable
+        self.name = name                                #Instance Variable  
         self.Id = Id
         # print("A student object created")
+  
+    def details(self):                                  #Instance Method
+        print("Name:", self.name, "Id:", self.Id)                                  
+         
             
 #variable = class_name()
 s1 = Student("Kobbla", 23)
 s2 = Student("Habla", 34)
-print(s1.Id) #23
 
-s1.Id = 33
-print(s1.Id) #33
+s1.Id = 67
+s1.details()
+s2.details()
+s1.details()
+s2.details()
+
+# print(s1.Id) #23
+
+# s1.Id = 33
+# print(s1.Id) #33
 
 
 
