fba330ccb3ed95646604b0029be842fb704778ef
tanvirulislam912 <mdtanvir1109040@gmail.com>
2023-12-05 16:18:47 +0600

    first commit

---
+++ Class Object Constructor Attributes Methods().py
@@ -0,0 +1,26 @@
+'''Class Objects Constructor Attributes Methods()'''
+
+
+# class Student:
+#     pass
+
+# #variable = class_name()
+# s1 = Student()
+# print(s1)
+
+
+
+class Student:
+
+    def __init__(self):                       #Constructor Created
+        print(self)
+        print("A student object created")
+            
+#variable = class_name()
+s1 = Student()
+s2 = Student()
+print('s1',s1)
+print('s2',s2)        
+
+      
+        
